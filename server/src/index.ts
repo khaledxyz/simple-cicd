d5b865376d84a3a01d192a4fd83dc8dfe674e280
khaledxyz <khaledhalloua@outlook.com>
2025-08-19 22:31:28 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ app.get("/health", (req: Request, res: Response) => {
 
 // example root route
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript + Express! v1");
+  res.send("Hello, TypeScript + Express! v2");
 });
 
 app.listen(PORT, () => {
